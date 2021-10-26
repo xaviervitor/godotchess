057b3ed9570047236f60b0c933022a8aff0c1214
Vitor Xavier <xavier1vitor@gmail.com>
2021-10-26 00:52:11 -0300

    Added a theme and changed icons

--- ui/HUD/FullscreenButton.gd
+++ ui/HUD/FullscreenButton.gd
@@ -1,19 +1,8 @@
 extends Button
 
-var maximize_icon = preload("res://assets/ui/maximize.png")
-var minimize_icon = preload("res://assets/ui/minimize.png")
-
-var button_state = false
-
 func _ready():
 	# warning-ignore:return_value_discarded
 	connect("button_up", self, "_on_FullscreenButton_button_up")
-	update_icon()
-	
+
 func _on_FullscreenButton_button_up():
 	OS.window_fullscreen = !OS.window_fullscreen
-	button_state = !button_state
-	update_icon()
-
-func update_icon():
-	icon = minimize_icon if button_state else maximize_icon
